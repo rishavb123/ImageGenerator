e393e9d8fe106a2136081da55b13009c84bf6a0e
Rishav Bhagat <rishavb123@gmail.com>
2020-07-23 16:00:41 -0400

    finished fixing pylint warnings

--- util/program.py
+++ util/program.py
@@ -1,14 +1,19 @@
+"""This file contains the Program class"""
 from multiprocessing import Process
 
 class Program:
+    """A class that fills contains methods that are useful for programs in this project to have"""
 
     def __init__(self, args):
         super().__init__()
         self.args = args
         self.process = Process(target=self.run)
+        self.ran = False
 
     def run(self):
-        print("Using the default run method. Please override this method to make this subclass functional.")
+        """The main method for this instance of the program class."""
+        self.ran = True
 
     def start(self):
+        """Runs the run method in a seperate process (enables multiprocessing)"""
         self.process.start()
