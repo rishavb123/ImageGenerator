91396430051290f74a6fee013eda2194c82c9653
Rishav Bhagat <rishavb123@gmail.com>
2020-07-23 16:29:31 -0400

    removed pylint comments

--- util/program.py
+++ util/program.py
@@ -1,8 +1,6 @@
-"""This file contains the Program class"""
 from multiprocessing import Process
 
 class Program:
-    """A class that fills contains methods that are useful for programs in this project to have"""
 
     def __init__(self, args):
         super().__init__()
@@ -11,9 +9,7 @@ class Program:
         self.ran = False
 
     def run(self):
-        """The main method for this instance of the program class."""
         self.ran = True
 
     def start(self):
-        """Runs the run method in a seperate process (enables multiprocessing)"""
         self.process.start()
