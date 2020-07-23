3a5eaebe59d1312003b7f5a295df8459ec793641
Rishav Bhagat <rishavb123@gmail.com>
2020-07-23 16:54:36 -0400

    copied over the main file template

---
+++ dataset/download_data.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(
+    os.path.realpath(
+        os.path.join(os.getcwd(), os.path.expanduser(__file__))
+    )
+)
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+
+import argparse
+
+parser = argparse.ArgumentParser(description='')
+
+args = vars(parser.parse_args())
