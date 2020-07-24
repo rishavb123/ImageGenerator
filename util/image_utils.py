fc1d58f1ff5137f60ff2f399db303b62560a1921
Rishav Bhagat <rishavb123@gmail.com>
2020-07-24 08:50:05 -0400

    minor update

--- util/image_utils.py
+++ util/image_utils.py
@@ -1,9 +1,9 @@
 import numpy as np
-import urllib
+import urllib.request
 import cv2
 
 def download_image_from_url(url):
-    resp = urllib.urlopen(url)
+    resp = urllib.request.urlopen(url)
     image = np.asarray(bytearray(resp.read()), dtype="uint8")
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
     return image
