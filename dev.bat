5d509a5c0851864bbcc5890d123761eeb7cb69c1
Rishav Bhagat <rishavb123@gmail.com>
2020-07-28 09:22:20 -0400

    reodering of lines

--- dev.bat
+++ dev.bat
@@ -1,4 +1,4 @@
 @echo off
-activate
-chrome "https://github.com/rishavb123/ImageGenerator" "https://www.youtube.com/watch?v=5qap5aO4i9A"
+chrome "https://github.com/rishavb123/ImageGenerator" "https://www.youtube.com/watch?v=5qap5aO4i9A" "https://www.flickr.com/services/apps/72157715219823141/"
 code .
+activate
