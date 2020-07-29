5f70f8257bf360fc3f4fb4d64de794ec10dc40a3
Rishav Bhagat <rishavb123@gmail.com>
2020-07-29 08:19:37 -0400

    added normailize method

--- util/image_utils.py
+++ util/image_utils.py
@@ -7,3 +7,6 @@ def download_image_from_url(url):
     image = np.asarray(bytearray(resp.read()), dtype="uint8")
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
     return image
+
+def normalize(images, output_range=(-1, 1), input_range=(0, 255)):
+    return (images - input_range[0]) * (output_range[1] - output_range[0]) / (input_range[1] - input_range[0]) + output_range[0]
