227792dde3de06d3d3c216e6e96fa3973b669528
Rishav Bhagat <rishavb123@gmail.com>
2020-07-29 09:35:14 -0400

    created pipeline class

---
+++ util/pipeline.py
@@ -0,0 +1,12 @@
+from multiprocessing import Process
+
+class Pipeline(Program):
+
+    def __init__(self, args, programs_classes=[]):
+        super().__init__(args)
+        self.programs = [p(args) for p in programs_classes]
+
+    def run(self):
+        super().run()
+        for program in self.programs:
+            program.run()
